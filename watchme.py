401041572f23443acac11ee8f7177744caa88640
Yash Shrivastava <yash.shrivastava77@gmail.com>
2023-04-06 15:56:43 +0530

    Implemented get extention function

--- watchme.py
+++ watchme.py
@@ -4,17 +4,22 @@ from collections import defaultdict
 from os.path import isfile, join
 from os import walk
 
+def get_extension(filename):
+    extentsion = filename.split(".")
+    return extentsion[-1]
+
 def get_time(file):
     fd = os.open(file, os.O_RDONLY)
     time = os.fstat(fd).st_mtime
+    os.close(fd)
     return time
 
 def get_all_paths(curr_path):
     paths = []
-    extentions_to_watch = {"cpp":1 , "ava":1, ".js":1, ".ts":1, "tsx":1, "jsx":1, "txt":1}
+    extentions_to_watch = {"cpp":1 , "java":1, "js":1, "ts":1, "tsx":1, "jsx":1, "txt":1}
     for (dirpath, dirname, files) in os.walk(curr_path):
         for file in files:
-            if(file[-3:] in extentions_to_watch):
+            if(get_extension(file) in extentions_to_watch):
                 paths.append(join(dirpath,file))
     return paths
 
